b948d03bf757b874359e343b67c06a29b103f5bb
Vukasin Jovanovic <jovanovicvukasin99@gmail.com>
2022-02-15 03:46:45 +0100

    Prikaz, pretraga i dodavanje sala

---
+++ src/main/java/com/ftn/Teretana/dao/SalaDAO.java
@@ -0,0 +1,16 @@
+package com.ftn.Teretana.dao;
+
+import java.util.List;
+
+import com.ftn.Teretana.model.Sala;
+
+public interface SalaDAO {
+	
+	public List<Sala> findAll();
+	public List<Sala> findOznakaSale(String oznakaSale);
+	public Sala findOne(Long id);
+	public void save(Sala sala);
+	public Sala findOne(String oznakaSale);
+
+
+}
